9586597c8798a9c4e6028c519bdb0f60ccb5c662
Qu <Quan0027@elec.techcollege.dk>
2019-09-23 11:02:46 +0200

    add return value to Action Random

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,7 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek"};
-            return View();
+            return View(movie);
         }
     }
 }
