f200feb5585a9d08e0df06a416f7a3edded712da
Qu <Quan0027@elec.techcollege.dk>
2019-09-23 10:56:20 +0200

    add data to MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Random()
         {
+            var movie = new Movie() {Name = "Shrek"};
             return View();
         }
     }
