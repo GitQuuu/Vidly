b17bb3338e75f10e383b95fadda4d7e856045902
Qu <Quan0027@elec.techcollege.dk>
2019-09-26 11:07:40 +0200

    Exercise 1 done

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,49 +12,28 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() {Name = "Shrek"};
+            var movies = GetMovies();
 
+            return View(movies);
+        }
 
-            List<Customer> customer = new List<Customer>
+        public ActionResult Details(int id)
+        {
+            var movies = GetMovies().SingleOrDefault(c => c.Id == id);
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
             };
-
-            RandomMovieViewModel viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customer
-            };
-
-            return View(viewModel);
-        }
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content($"{year}/{month}");
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format($"pageIndex {pageIndex} & sortBy {sortBy}"));
         }
 
     }
