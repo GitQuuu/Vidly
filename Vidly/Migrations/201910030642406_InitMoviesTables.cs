b8f20efc9e59368f30670d4315024f53640929ae
Qu <Quan0027@elec.techcollege.dk>
2019-10-03 08:52:59 +0200

    Init Movie Table and creating movies columns
    Creating movie table in IdentityModels and creating table columns with data annotations

---
+++ Vidly/Migrations/201910030642406_InitMoviesTables.cs
@@ -0,0 +1,30 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitMoviesTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Genre = c.String(nullable: false, maxLength: 50),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        DateAdded = c.DateTime(nullable: false),
+                        NumberInStock = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,20 @@ namespace Vidly.Models
 {
     public class Movie
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int NumberInStock { get; set; }  
     
     }
 }
