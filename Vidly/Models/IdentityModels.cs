e769c3d6df5e66cfac31d0533a4fe79df6b3ce72
Qu <Quan0027@elec.techcollege.dk>
2019-10-15 08:14:46 +0200

    InitGenreTableInEFAndRemovingRequiredFromMovieId

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -24,6 +24,8 @@ namespace Vidly.Models
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipType { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -8,15 +8,18 @@ namespace Vidly.Models
 {
     public class Movie
     {
-        [Required]
+        
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
         
         public DateTime ReleaseDate { get; set; }
+        
         [Required]
         public DateTime DateAdded { get; set; }
+        
         [Required]
         public int NumberInStock { get; set; }  
     
