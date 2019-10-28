204da505d3b67aef6a482ffa4dd4f51a8574830e
Qu <Quan0027@elec.techcollege.dk>
2019-10-28 12:34:08 +0100

    Data Transfer Objects
    https://codewithmosh.com/courses/222293/lectures/3684148

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customers name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MemberShipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        [Column(TypeName = "date")]
+        public DateTime? Birthdate { get; set; }
+    }
+}
