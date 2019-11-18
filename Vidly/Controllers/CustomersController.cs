390536c0555de38c106eb20f6698cea5920d553f
Qu <Quan0027@elec.techcollege.dk>
2019-11-18 12:43:20 +0100

    Establish connection to DB  and display customer in its Index View
    CustomersController and Index View of Customer Model

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool dispose)
+        {
+            _context.Dispose();
+        }
+        
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customerInDb = _context.Customers.ToList();
+
+            return View(customerInDb);
+        }
+    }
+}
