6d5dbe25ff2a44cec0ccf162e50b1b21ea2fef5f
Qu <Quan0027@elec.techcollege.dk>
2019-11-20 09:27:10 +0100

    Actionlink to Delete Action now works
    Was missing a routevalue in Customer Index View

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -29,13 +31,20 @@ namespace Vidly.Controllers
             return View(customerInDb);
         }
 
-        public ActionResult Delete(int Id)
+        
+        public ActionResult Delete(int id)
         {
-            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            Customer customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            if (customerInDb.Id)
+            if (customerInDb == null)
             {
-                
+
+                throw new HttpRequestException(HttpStatusCode.NotFound.ToString());
+            }
+            else
+            {
+                _context.Customers.Remove(customerInDb);
+                _context.SaveChanges();
             }
 
             return RedirectToAction("Index", "Customers");
