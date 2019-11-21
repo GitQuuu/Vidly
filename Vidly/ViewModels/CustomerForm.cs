335892b1143d318d4d21dacbaf22cc566aaa07a5
Qu <Quan0027@elec.techcollege.dk>
2019-11-21 12:28:05 +0100

    add new Domain Model CusterForm for our viewModel

---
+++ Vidly/ViewModels/CustomerForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerForm
+    {
+        public Customer Customer { get; set; }
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+    }   
+}
